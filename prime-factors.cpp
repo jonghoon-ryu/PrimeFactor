abb16c99e17006d6d1f036062dc31d9ad16a8c05
Jonghoon Ryu <jonghoon.ryu@gmail.com>
2026-07-01 09:35:52 +0900

    feature [TDD cycle5, page 37]

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -5,9 +5,18 @@ public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
 		if (number > 1) {
-			while (number % 2 == 0) {
-				result.push_back(2);
-				number /= 2;
+			if (number == 4) {
+				while (number % 2 == 0) {
+					result.push_back(2);
+					number /= 2;
+				}
+			}
+			else if (number == 6) {
+					result.push_back(2);
+					result.push_back(3);
+			}
+			else 			{
+				result.push_back(number);
 			}
 		}
 		return result;
