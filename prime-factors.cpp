cd1ad33ffc42b48a06ace2ce5b909bff27059d98
Jonghoon Ryu <jonghoon.ryu@gmail.com>
2026-07-01 09:08:34 +0900

    feature [TDD cycle2, page 27]

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -2,7 +2,11 @@
 
 class PrimeFactors {
 public:
-	std::vector<int> of(int n) {
-		return {};
+	std::vector<int> of(int number) {
+		std::vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
+		return result;
 	}
 };
--- prime-factors_test.cpp
+++ prime-factors_test.cpp
@@ -1,12 +1,21 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-//TEST(PrimeFactors, PrimeTest) {
-//	EXPECT_EQ(1, 1);
-//}
+using namespace testing;
+class PrimeFixture : public Test {
+public:
+	PrimeFactors prime_factor;
+	std::vector<int> expected;
+};
 
-TEST(PrimeFactors, Of1) {
+TEST(PrimeFixture, Of1) {
 	PrimeFactors prime_factor;
 	std::vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFixture, Of2) {
+	PrimeFactors prime_factor;
+	std::vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
