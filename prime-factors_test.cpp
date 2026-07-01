cf0b782f6de86a5236cf45946480479c705e485b
Jonghoon Ryu <jonghoon.ryu@gmail.com>
2026-07-01 09:12:28 +0900

    feature [TDD cycle2, page 27] : more slight change

--- prime-factors_test.cpp
+++ prime-factors_test.cpp
@@ -8,14 +8,12 @@ public:
 	std::vector<int> expected;
 };
 
-TEST(PrimeFixture, Of1) {
-	PrimeFactors prime_factor;
-	std::vector<int> expected = {};
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFixture, Of2) {
-	PrimeFactors prime_factor;
-	std::vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = { 2 };
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
